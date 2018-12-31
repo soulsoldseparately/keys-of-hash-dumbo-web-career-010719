fdb984099904c51b63b04595938476abd69faf70
Daniel Toma <danieltoma@ubalumni.com>
2018-12-31 20:41:01 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -9,10 +9,14 @@ def animals
 end 
 
 class Hash
+  #--need the splat symbol with unknown # of arguments--#
   def keys_of(*arguments)
     array = []
+    
     self.each do |key, value|
+      
       arguments.each do |i|
+
         if i == value
         array.push(key)
         end
